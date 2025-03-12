6021e13d7ddcb88e13e378098d673b5ab46504ff
davorce <cepindavor@gmail.com>
2025-03-12 20:00:32 +0100

    update1

--- src/Zadatak1/Zadatak1.java
+++ src/Zadatak1/Zadatak1.java
@@ -4,7 +4,6 @@ public class Zadatak1 {
     public static void main(String[] args) {
 
         StringBuilder b = new StringBuilder();
-        b.append("\n****** Zadatak 1 ******");
         b.append("\n\n\t   J    a   v     v  a   ");
         b.append("\n\t   J   a a   v   v  a a  ");
         b.append("\n\tJ  J  aaaaa   V V  aaaaa ");
