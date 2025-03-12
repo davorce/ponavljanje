5c0a8ff93d2cb2196826f69833ecddff41060030
davorce <cepindavor@gmail.com>
2025-03-12 20:00:52 +0100

    update1

--- src/Zadatak1/Zadatak1.java
+++ src/Zadatak1/Zadatak1.java
@@ -4,10 +4,10 @@ public class Zadatak1 {
     public static void main(String[] args) {
 
         StringBuilder b = new StringBuilder();
-        b.append("\n\n\t   J    a   v     v  a   ");
-        b.append("\n\t   J   a a   v   v  a a  ");
-        b.append("\n\tJ  J  aaaaa   V V  aaaaa ");
-        b.append("\n\t JJ  a     a   V  a     a");
+        b.append("\n   J    a   v     v  a   ");
+        b.append("\n   J   a a   v   v  a a  ");
+        b.append("\nJ  J  aaaaa   V V  aaaaa ");
+        b.append("\n JJ  a     a   V  a     a");
         System.out.println(b.toString());
     }
 }
